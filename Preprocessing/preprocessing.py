06cb839035558abd6be3ddbe6158b8ffdda19ec6
Jumana <gomana.akram1910@gmail.com>
2026-05-08 13:31:27 +0300

    Update preprocessing.py

--- Preprocessing/preprocessing.py
+++ Preprocessing/preprocessing.py
@@ -6,11 +6,7 @@ import joblib
 from sklearn.preprocessing import LabelEncoder
 from sklearn.preprocessing import  MinMaxScaler
 from sklearn.preprocessing import  OneHotEncoder
-import matplotlib.pyplot as plt
-import seaborn as sns
 
-sns.set_style("whitegrid")
-plt.rcParams["figure.figsize"] = (8,5)
 
 
 encoders={}
@@ -164,42 +160,10 @@ if __name__ == "__main__":
 
     processed_data.to_csv("Preprocessing/processed_train_data.csv", index=False)
     processed_data_test.to_csv("Preprocessing/processed_test_data.csv", index=False)
-    # After drop_duplicates for train:
-    processed_data.drop_duplicates(inplace=True)
-    processed_data.reset_index(drop=True, inplace=True)  # ← add this
-
-    # After drop_duplicates for test:
-    processed_data_test.drop_duplicates(inplace=True)
-    processed_data_test.reset_index(drop=True, inplace=True)  # ← add this
+  
 
 
-    # ///////
-    num_cols = ["age","fnlwgt","education-num","capital-gain","capital-loss","hours-per-week"]
 
-    for col in num_cols:
-        plt.figure()
-        sns.histplot(processed_data[col], kde=True)
-        plt.title(f"Distribution of {col}")
-        plt.xlabel(col)
-        plt.ylabel("Count")
-        plt.show()
-
-    plt.figure()
-    sns.countplot(x="Income", data=processed_data)
-    plt.title("Target Distribution (Income)")
-    plt.xlabel("Income")
-    plt.ylabel("Count")
-    plt.show()
-
-    print("Target :")
-    print(processed_data["Income"].value_counts(normalize=True))
-
-    plt.figure(figsize=(10,8))
-    corr = processed_data[num_cols].corr()
-
-    sns.heatmap(corr, annot=True, cmap="coolwarm")
-    plt.title("Correlation Heatmap (Numerical Features)")
-    plt.show()
 
     joblib.dump(encoders, "encoders.pkl")
     joblib.dump(onehotencoder, "onehotencoder.pkl")
